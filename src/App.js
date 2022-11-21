8e12596ea3aa5aa9a99c39bd465b201302fc5f6b
ramontcruz <115509915+ramontcruz@users.noreply.github.com>
2022-11-21 15:20:30 -0300

    ListagemPage configurada para listagem geral de registros

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import ListagemPage from './pages/ListagemPage';
+import CadastroPage from './pages/CadastroPage';
+import DetalhesPage from './pages/DetalhesPage';
+import EditarPage from './pages/EditarPage';
+
+
 
 function App() {
   return (
     <div className="App">
-    <h1>Teste escrito por Rodrigo</h1>
-      
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path="/new-item" element={<CadastroPage />} />
+        <Route path="/items" element={<ListagemPage />} />
+        <Route path="/item/editar/:idItem" element={<EditarPage />} />
+        <Route path={"/item/:idItem"} element={<DetalhesPage />} />
+      </Routes>
     </div>
   );
 }
